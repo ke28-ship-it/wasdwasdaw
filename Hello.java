95c0a079b6991c4cdf210c155d80e6eaedce59cc
Katz <katzelo28@7hills.org>
2026-09-22 10:17:01 -0400

    Added Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("there's \n outside of my house");
+		System.out.print("there isn't any \n's outside of my house");
+	}
+}
